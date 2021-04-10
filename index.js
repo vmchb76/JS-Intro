e6ef7a083d30d4047bc7677a3a17de1a0ad4e510
Victor Chavarria <victorm.chavarria@yahoo.com.mx>
2021-04-10 13:17:52 -0500

    Usamos la programacion funcional para obtener los nombres de los equipos (map) y pintarlos por pantalla (forEach)

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ import { premierLeagueTeams } from './teams.js'
 const config = {pointsPerWin : 2}
 const premier = new FootballLeague('Premier League', premierLeagueTeams, config)
 
-     for(const team of premier.teams) {
-         console.log(team)
-     }
+    /*function getTeamName(team) {
+        console.log('Voy a conertir:', team.name)
+        return team.name
+    }*/
+
+    /*const getTeamName = function(team) {
+          return team.name 
+    }*/
+
+    const teamNames = premier.teams.map(team => team.name)
+    
+    teamNames.forEach(function (equipo) {
+        console.log(equipo)
+       }
+    )
+
+
+    //console.log(premier.teams)
+    //console.log(teamNames)
+
+
+    // Esto hace el map
+    /*
+    premier.teams.map = function(funcionAejecutarConCadaElementoDelArray) {
+        const result = []
+        for(const team of this.teams) {
+            const newTeam = funcionAejecutarConCadaElementoDelArray(team)
+            result.push(newTeam)
+        }
+        return result
+    } */
+
+   /*
+    for(const teamName of teamNames) {
+        console.log(teamName)
+    }
+   */
+
+
+
+    /* for(const team of premier.teams) {
+         console.log(team.name)
+     } */
